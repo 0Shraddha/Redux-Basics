0ec179effb0cbc5e2b2feb482e04396b93f1d1d0
shraddha dongol <shraddhadongol59@gmail.com>
2025-07-27 22:43:36 +0545

    gitingore file created

---
+++ src/store/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'redux'
+
+const counterReducer = (state = {counter : 0}, action) => {
+    if(action.type === 'increment') {
+        return { 
+            counter: state.counter + 1 
+        }
+    }
+    
+    if(action.type === 'decrement') {
+        return { 
+            counter: state.counter - 1 
+        }
+
+
+    }
+
+    return state;
+
+}
+
+//connect react app to redux store
+
+const store = createStore(counterReducer);
+export default store;
