11385661e6f7700d966f0c08fca1d3341a83e951
shraddha dongol <shraddhadongol59@gmail.com>
2025-07-28 22:48:21 +0545

    revised redux basics and started the redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -8,24 +8,33 @@ const Counter = () => {
   // Access the counter value from the Redux store
   // Use useSelector to get the current state
   const counter = useSelector(state => state.counter);
+  const showCounter = useSelector(state => state.showCounter)
 
   const incrementHandler = () => {
     dispatch({type: 'increment'});
+  }
+
+  const increaseHandler = () => {
+    dispatch({type: 'increase', amount: 5});
   }
 
   const decrementHandler = () => {
     dispatch({type: 'decrement'});
   }
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({type : 'toggler'})
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      { showCounter && <div className={classes.value}>{counter}</div> }
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
+
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
